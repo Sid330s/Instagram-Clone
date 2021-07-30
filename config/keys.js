0e9c17dc0a07be26a22d2e5bfc62ba0aafa32f16
sidd <siddharthvaidon@gmail.com>
2021-07-30 22:13:34 +0530

    Connected to Mongodb atlas

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://siddharth:8tPrBU5sszr4BtG@cluster0.iqs0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    HOST:"http:localhost:3000"
+}
--- server/app.js
+++ server/app.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
+const mongoose  = require('mongoose')
+const PORT = process.env.PORT || 5000
+const {MONGOURI} = require('./config/keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongodb")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+
 
 app.listen(PORT,()=>{
-    console.log("Hello World!, server is running on",PORT)
+    console.log("server is running on",PORT)
 })

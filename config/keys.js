9a78a92534f0bd308b49a2191fa6b5f8c52af381
sidd <siddharthvaidon@gmail.com>
2021-08-05 12:45:01 +0530

    LOG IN Added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports={
     MONGOURI:"mongodb+srv://siddharth:8tPrBU5sszr4BtG@cluster0.iqs0h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"ewewewewer",
     HOST:"http:localhost:3000"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../config/keys')
 
 router.post('/signup',(req,res)=>{
   const {name,email,password,pic} = req.body
@@ -38,5 +39,35 @@ router.post('/signup',(req,res)=>{
     console.log(err)
   })
 })
+
+
+router.post('/signin',(req,res)=>{
+    const {email,password} = req.body
+    if(!email || !password){
+       return res.status(422).json({error:"PLEASE ADD ALL THE FIELDS !!!"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
+        if(!savedUser){
+           return res.status(422).json({error:"INVALID EMAIL OR PASSWORD !!!"})
+        }
+        bcrypt.compare(password,savedUser.password)
+        .then(doMatch=>{
+            if(doMatch){
+               const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+               console.log(token)
+               const {_id,name,email,followers,following,pic} = savedUser
+               res.json({token,user:{_id,name,email,followers,following,pic}})
+            }
+            else{
+                return res.status(422).json({error:"INVALID EMAIL OR PASSWORD !!!"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
+    })
+})
+
 
 module.exports = router

fb8f79d5a211e9de058e03171f3bfbd298aa4cc6
sidd <siddharthvaidon@gmail.com>
2021-08-12 20:40:16 +0530

    Profile Page and Related CSS added

---
+++ client/src/components/Reset.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import {toast} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure()
+
+const Reset  = ()=>{
+    const history = useHistory()
+    const [email,setEmail] = useState("")
+    const PostData = ()=>{
+        if(!/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(email)){
+            return toast('INVALID EMAILID',{position: toast.POSITION.TOP_LEFT});
+        }
+        fetch('/reset-password',{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+           if(data.error){
+              return toast(data.error,{position: toast.POSITION.TOP_LEFT})
+           }
+           else{
+               toast("PLEASE CHECK YOUR MAIL",{position: toast.POSITION.TOP_LEFT})
+               history.push('/signin')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+   return (
+      <div className="mycard">
+          <div className="card auth-card">
+            <h2>Instagram</h2>
+            <input
+            type="text"
+            placeholder="email"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            />
+            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+            onClick={()=>PostData()}
+            >
+               reset password
+            </button>
+
+
+        </div>
+      </div>
+   )
+}
+
+
+export default Reset

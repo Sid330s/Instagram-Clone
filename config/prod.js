fa6df5b1a68745169d937d202197dc13a9625af2
Siddharth Kale <52232241+Sid330s@users.noreply.github.com>
2021-10-20 18:48:26 +0530

    Update prod.js
    JWT Secret Evironment variable name changed

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports={
     MONGOURI:process.env.MONGOURI,
-    JWT_SECRET:process.env.JWT_SEC,
+    JWT_SECRET:process.env.JWT_SECRET,
     SENDGRID_API:process.env.SENDGRID_API,
     HOST:process.env.HOST
 }
